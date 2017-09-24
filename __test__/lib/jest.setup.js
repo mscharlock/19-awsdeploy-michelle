05dca630570ec5b2e048b33aaa582180d7e10612
mscharlock <michelle.scharlock@gmail.com>
2017-09-24 16:40:43 -0700

    Envs

--- __test__/lib/jest.setup.js
+++ __test__/lib/jest.setup.js
@@ -1,4 +1,4 @@
 'use strict';
 
 //make sure jest knows where dotenv file lives for the test
-require('dotenv').config({path: `${__dirname}/../.test.env`});
+require('dotenv').config({path: `${__dirname}/../test.env`});
